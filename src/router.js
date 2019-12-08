2d7dd69b8ee3b3689ad779a3d38bb73bea335465
nlho@yahoo.com <nlho@yahoo.com>
2019-12-08 17:49:26 +0800

    ZH encoding

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import RouterView from "./views/RouterView.vue";
 import { i18n } from "./main.js";
+
 Vue.use(Router);
 export default new Router({
   mode: "history",
@@ -14,31 +15,32 @@ export default new Router({
            component: RouterView,
            beforeEnter(to, from, next) {
              const lang = to.params.lang;
-             if (!["en", "en"].includes(lang)) return next("en");
+             if (!["en", "zh-Hant", "zh-Hans"].includes(lang)) return next("en");
              if (i18n.locale !== lang) {
                i18n.locale = lang;
              }
              return next();
-     },
-
-           children: [
-                       {
-                         path: "home",
-                         name: "home",
-                         component: Home
-                       },
-                       {
-                         path: "about",
-                         name: "about",
-                         // route level code-splitting
-                         // this generates a separate chunk (about.[hash].js) for this route
-                         // which is lazy-loaded when the route is visited.
-                         ////component: () =>
-                         ////  import(/* webpackChunkName: "about" */ "./views/About.vue")
-               }
-                     ]
          },
-         {
+
+         children: [
+             {
+               path: "home",
+               name: "home",
+               component: Home
+             },
+             {
+               path: "about",
+               name: "about",
+               // route level code-splitting
+               // this generates a separate chunk (about.[hash].js) for this route
+               // which is lazy-loaded when the route is visited.
+               ////component: () =>
+               ////  import(/* webpackChunkName: "about" */ "./views/About.vue")
+             }
+         ]
+       },
+
+       {
          path: "*",
          redirect: "/en"
          }
